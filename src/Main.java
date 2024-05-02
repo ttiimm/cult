349e24ac133d9b0df81ebeed2b4886ad1c3294b8
Tim Likarish <likarish@gmail.com>
2024-05-02 16:52:58 -0400

    [Cult] WIP of ` build` subcommand
    Can call cult with the build argument and print the contents of the
    Cult.toml file if it exists. When missing outputs as error to stderr.
    
    If no subcommand is passed, then the usage is printed.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length < 1) {
+            usage();
+            System.exit(1);
+        }
+
+        switch (args[0]) {
+            case "build":
+                build();
+                break;
+            default:
+                usage();
+        }
+    }
+
+    private static void usage() {
         System.out.println("A simple Java build tool");
+    }
+
+    private static void build() {
+        Path toml = Paths.get("Cult.toml");
+        try {
+            System.out.println(Files.readAllLines(toml));
+        } catch (IOException e) {
+            String pwd = System.getProperty("user.dir");
+            System.err.println(STR."error: could not find `Cult.toml` in `\{pwd}`");
+        }
     }
 }
